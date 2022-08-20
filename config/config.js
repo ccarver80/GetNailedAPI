ce49bece56a6a47aa44698409705e71877cb4211
ccarver80 <ckc_80@outlook.com>
2022-08-20 14:38:16 -0600

    deleted env file

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
     host     : process.env.RDS_HOSTNAME,
-    user     : "ccarver80",
+    user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres"
   },
   test: {
     host     : process.env.RDS_HOSTNAME,
-    user     : "ccarver80",
+    user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres",
   },
   production: {
     host     : process.env.RDS_HOSTNAME,
-    user     : "ccarver80",
+    user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres"
