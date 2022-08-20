4adc9fa030ffedff4a631c7564bf2cde5efb4094
ccarver80 <ckc_80@outlook.com>
2022-08-20 14:34:22 -0600

    added username

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   development: {
     host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
+    user     : "ccarver80",
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres"
   },
   test: {
     host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
+    user     : "ccarver80",
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres",
   },
   production: {
     host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
+    user     : "ccarver80",
     password : process.env.RDS_PASSWORD,
     port     : process.env.RDS_PORT,
     dialect : "postgres"
