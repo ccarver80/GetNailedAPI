daac275689f280841540ee79cc5b8444b8b0ceae
ccarver80 <ckc_80@outlook.com>
2022-08-20 14:54:51 -0600

    fixed sequelize

--- models/index.js
+++ models/index.js
@@ -8,14 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  console.log("IFFFFFFFFF=====================")
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  console.log("ELLLSSSEEEEEEEEEEEEE===========================")
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
 
 fs
   .readdirSync(__dirname)
