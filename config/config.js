517d99af521c4f6d0eea7e3f61ba27027655c06c
ccarver80 <ckc_80@outlook.com>
2022-08-20 14:30:37 -0600

    added env to config

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    dialect : "postgres"
+  },
+  test: {
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    dialect : "postgres",
+  },
+  production: {
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    dialect : "postgres"
+  }
+};
