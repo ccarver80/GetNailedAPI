587e94011324b5e73c9bf649a82d17cc995cd9c2
ccarver80 <ckc_80@outlook.com>
2022-08-19 20:45:38 -0600

    addded dialect

---
+++ config/config.js
@@ -0,0 +1,25 @@
+module.exports = 
+{
+  "development": {
+    "username": "postgres",
+    "password": null,
+    "database": "startup_quotes",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": null,
+    "database": "startup_quotes_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.RDS_USERNAME || '',
+    "password": process.env.RDS_PASSWORD || '',
+    "database": process.env.RDS_DB_NAME || "startup_quotes",
+    "host": process.env.RDS_HOSTNAME || '',
+    "port": process.env.RDS_PORT || '5432',
+    "dialect": "postgres"
+  }
+}
