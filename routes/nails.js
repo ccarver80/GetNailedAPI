c1889f3752507869a9b8d6fb13a7fa497dff5e96
ccarver80 <ckc_80@outlook.com>
2022-08-21 09:59:16 -0600

    cors options

--- routes/nails.js
+++ routes/nails.js
@@ -51,7 +51,17 @@ router.post('/nails', async(req, res) => {
 router.post('/nail-custom', async(req, res) => {
     try{
 
-      
+    
+
+        let file;
+        let photo; 
+        if(req.files === null){
+            file = "none selected"
+            photo = "none selected"
+        }else {
+            file = req.files.style2.data
+            
+        }
 
       
 
@@ -61,7 +71,7 @@ router.post('/nail-custom', async(req, res) => {
             lastName: req.body.lastName,
             email: req.body.email,
             style1: nails.findByPk(req.body.style1),
-            style2: req.files.style2.data,
+            style2: photo,
             shape: req.body.shape,
             length: req.body.length,
             rt: req.body.rt,
@@ -77,54 +87,54 @@ router.post('/nail-custom', async(req, res) => {
             specialRequests: req.body.specialRequests,
         })
 
-        // if(file != "none selected"){
-        //     photo = `<img style='height:500px;' src=${process.env.HOST}nail-custom/${newCustom.id}/>`
-        // }
+        if(file != "none selected"){
+            photo = `<img style='height:500px;' src=${process.env.HOST}nail-custom/${newCustom.id}/>`
+        }
 
-        // let transporter = nodemailer.createTransport({
-        //     host: "smtp-mail.outlook.com",
-        //     port: 587,
-        //     secure: false, // true for 465, false for other ports
-        //     auth: {
-        //       user: process.env.EMAIL, // generated ethereal user
-        //       pass: process.env.PASSWORD, // generated ethereal password
-        //     },
-        //   });
+        let transporter = nodemailer.createTransport({
+            host: "smtp-mail.outlook.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: process.env.EMAIL, // generated ethereal user
+              pass: process.env.PASSWORD, // generated ethereal password
+            },
+          });
 
-        //   let customer = await transporter.sendMail({
-        //     from: '"Mandi Watson" <get_nailed_art@outlook.com>', // sender address
-        //     to: req.body.email, // list of receivers
-        //     subject: "Thank You for your request for a custom order!", // Subject line
-        //     text: "Thank you for your interest in a custom set! I will review the order and get back to you on price shortly", // plain text body
-        //     html: "<b>Thank you for your interest in a custom set! I will review the order and get back to you on price shortly</b>", // html body
-        //   });
+          let customer = await transporter.sendMail({
+            from: '"Mandi Watson" <get_nailed_art@outlook.com>', // sender address
+            to: req.body.email, // list of receivers
+            subject: "Thank You for your request for a custom order!", // Subject line
+            text: "Thank you for your interest in a custom set! I will review the order and get back to you on price shortly", // plain text body
+            html: "<b>Thank you for your interest in a custom set! I will review the order and get back to you on price shortly</b>", // html body
+          });
 
-        //   let order = await transporter.sendMail({
-        //     from: '"New CUSTOM Order" <get_nailed_art@outlook.com>', // sender address
-        //     to: process.env.EMAIL, // list of receivers
-        //     subject: "You Have A New Custom Order!", // Subject line
+          let order = await transporter.sendMail({
+            from: '"New CUSTOM Order" <get_nailed_art@outlook.com>', // sender address
+            to: process.env.EMAIL, // list of receivers
+            subject: "You Have A New Custom Order!", // Subject line
             
-        //     html: `<h1>You have a new custom order from! ${req.body.firstName} ${req.body.lastName}</h1>
-        //             <p><b>Email:</b>${req.body.email}</p>
-        //             <p><b>Style picked:</b><img src=${process.env.HOST}nails/${req.body.style1}/><p>
-        //             <p><b>Submitted Photo:</b>${photo}</p>
-        //             <p><b>Shape:</b>${req.body.shape}</p>
-        //             <p><b>Length:</b>${req.body.length}</p>
-        //             <h1>Nail Size:</h1>
-        //             <p><b>Right Thumb:</b> ${req.body.rt}</p>
-        //             <p><b>Right Index:</b> ${req.body.ri}</p>
-        //             <p><b>Right Middle:</b> ${req.body.rm}</p>
-        //             <p><b>Right Ring:</b> ${req.body.rm}</p>
-        //             <p><b>Right Pinky:</b> ${req.body.rp}</p>
-        //             <p><b>Left Thumb:</b> ${req.body.lt}</p>
-        //             <p><b>Left Index:</b> ${req.body.li}</p>
-        //             <p><b>Left Middle:</b> ${req.body.lm}</p>
-        //             <p><b>Left Ring:</b> ${req.body.lr}</p>
-        //             <p><b>Left Pinky:</b> ${req.body.lp}</p>
-        //             <p><b>Special Requests:</b> ${req.body.specialRequests}</p>
+            html: `<h1>You have a new custom order from! ${req.body.firstName} ${req.body.lastName}</h1>
+                    <p><b>Email:</b>${req.body.email}</p>
+                    <p><b>Style picked:</b><img src=${process.env.HOST}nails/${req.body.style1}/><p>
+                    <p><b>Submitted Photo:</b>${photo}</p>
+                    <p><b>Shape:</b>${req.body.shape}</p>
+                    <p><b>Length:</b>${req.body.length}</p>
+                    <h1>Nail Size:</h1>
+                    <p><b>Right Thumb:</b> ${req.body.rt}</p>
+                    <p><b>Right Index:</b> ${req.body.ri}</p>
+                    <p><b>Right Middle:</b> ${req.body.rm}</p>
+                    <p><b>Right Ring:</b> ${req.body.rm}</p>
+                    <p><b>Right Pinky:</b> ${req.body.rp}</p>
+                    <p><b>Left Thumb:</b> ${req.body.lt}</p>
+                    <p><b>Left Index:</b> ${req.body.li}</p>
+                    <p><b>Left Middle:</b> ${req.body.lm}</p>
+                    <p><b>Left Ring:</b> ${req.body.lr}</p>
+                    <p><b>Left Pinky:</b> ${req.body.lp}</p>
+                    <p><b>Special Requests:</b> ${req.body.specialRequests}</p>
                     
-        //               ` // html body
-        //   });
+                      ` // html body
+          });
          
         res.status(201).json({message: "Successfully submitted!! You will be receiving a email shortly, Please check your spam/junk folder."})
     }catch(err){
