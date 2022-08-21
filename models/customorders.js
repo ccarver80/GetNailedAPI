d382531dbc18ad31f7383e2677df89f93c834e3c
ccarver80 <ckc_80@outlook.com>
2022-08-21 07:03:01 -0600

    undid null

--- models/customorders.js
+++ models/customorders.js
@@ -17,67 +17,21 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
-    style1: {
-      type: DataTypes.BLOB,
-      allowNull: true,
-    },
-
-    style2: {
-      type: DataTypes.BLOB,
-      allowNull: true,
-    },
-    shape: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    length: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    rt: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    ri: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    rm: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    rr: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    rp: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    lt: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    li: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    lm: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    lr: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    lp: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    specialRequests: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
+    style1: DataTypes.BLOB,
+    style2: DataTypes.BLOB,
+    shape: DataTypes.STRING,
+    length: DataTypes.STRING,
+    rt: DataTypes.INTEGER,
+    ri: DataTypes.INTEGER,
+    rm: DataTypes.INTEGER,
+    rr: DataTypes.INTEGER,
+    rp: DataTypes.INTEGER,
+    lt: DataTypes.INTEGER,
+    li: DataTypes.INTEGER,
+    lm: DataTypes.INTEGER,
+    lr: DataTypes.INTEGER,
+    lp: DataTypes.INTEGER,
+    specialRequests: DataTypes.TEXT,
     
   }, {
     sequelize,
